813a577b939f649952e9463d2dd2e7d994e79510
Dom Delnano <ddelnano@gmail.com>
2024-06-24 15:52:41 -0700

    Update license checker to verify Cosmic proprietery license is added and fix existing files (#7)
    * Update license checker to verify Cosmic proprietery license is added and fix existing files
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    * Debug what python is being used. Remove logo
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    * Remove change from debugging
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    ---------
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- private/testing/deploy_secrets.sh
+++ private/testing/deploy_secrets.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Copyright © 2024- Cosmic Observe, Inc.
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of Cosmic Observe, Inc. and its suppliers,
+# if any.
+#
+# SPDX-License-Identifier: Proprietary
+
 # get directory of this script
 config_dir="$(dirname "$0")"
 
