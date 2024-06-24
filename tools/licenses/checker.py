46fc3ccbf76d10d4eebc20ef9358bdb2f1efe64c
Dom Del Nano <ddelnano@gmail.com>
2024-06-24 21:46:28 +0000

    Update license checker to verify Cosmic proprietery license is added and fix existing files
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- tools/licenses/checker.py
+++ tools/licenses/checker.py
@@ -66,19 +66,12 @@ mit_license_header = '''
 '''
 
 private_license_header = '''
- Copyright © 2018- Pixie Labs Inc.
- Copyright © 2020- New Relic, Inc.
+ Copyright © 2024- Cosmic Observe, Inc.
  All Rights Reserved.
 
  NOTICE:  All information contained herein is, and remains
- the property of New Relic Inc. and its suppliers,
- if any.  The intellectual and technical concepts contained
- herein are proprietary to Pixie Labs Inc. and its suppliers and
- may be covered by U.S. and Foreign Patents, patents in process,
- and are protected by trade secret or copyright law. Dissemination
- of this information or reproduction of this material is strictly
- forbidden unless prior written permission is obtained from
- New Relic, Inc.
+ the property of Cosmic Observe, Inc. and its suppliers,
+ if any.
 
  SPDX-License-Identifier: Proprietary
 '''
