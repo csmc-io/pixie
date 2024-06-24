a38012560a25914a8ed0d9ed4fea092309f583c7
Dom Delnano <ddelnano@gmail.com>
2024-06-24 15:52:41 -0700

    Update license checker to verify Cosmic proprietery license is added and fix existing files (#7)
    * Update license checker to verify Cosmic proprietery license is added and fix existing files
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    * Debug what python is being used. Remove logo
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    * Remove change from debugging
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>
    
    ---------
    
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/ui/src/configurables/private/copyright.tsx
+++ src/ui/src/configurables/private/copyright.tsx
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2024- Cosmic Observe, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Cosmic Observe, Inc. and its suppliers,
+ * if any.
+ *
+ * SPDX-License-Identifier: Proprietary
+ */
+
 import * as React from 'react';
 
 export const Copyright = React.memo(() => (
