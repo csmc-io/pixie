e69082820f8630e5da8cc4b40f85595d99d26465
Dom Del Nano <ddelnano@gmail.com>
2024-06-25 06:37:38 +0000

    Add prod secrets and sops config
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

---
+++ private/prod/deploy_secrets.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright © 2024- Cosmic Observe, Inc.
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of Cosmic Observe, Inc. and its suppliers,
+# if any.
+#
+# SPDX-License-Identifier: Proprietary
+
+# get directory of this script
+config_dir="$(dirname "$0")"
+
+kubectl -n plc apply -f "${config_dir}/announcement_config.yaml"
+sops -d "${config_dir}/segment_config.yaml" | kubectl -n plc apply -f -
+sops -d "${config_dir}/sentry_config.yaml" | kubectl -n plc apply -f -
+kubectl -n plc apply -f "${config_dir}/auth_connector_config.yaml"
+sops -d "${config_dir}/errors_config.yaml" | kubectl -n plc apply -f -
+sops -d "${config_dir}/ld_config.yaml" | kubectl -n plc apply -f -
+sops -d "${config_dir}/oauth_config.yaml" | kubectl -n plc apply -f -
+sops -d "${config_dir}/service_tls_certs.yaml" | kubectl -n plc apply -f -
