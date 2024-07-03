32a995e96f360c9be07992b6fabc0967be0a1145
Dom Del Nano <ddelnano@gmail.com>
2024-07-03 17:59:27 +0000

    Switch back to monochrome logo
    Signed-off-by: Dom Del Nano <ddelnano@gmail.com>

--- src/ui/src/configurables/private/logo.tsx
+++ src/ui/src/configurables/private/logo.tsx
@@ -14,8 +14,7 @@ import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { createStyles, makeStyles } from '@mui/styles';
 
-import * as darkLogo from 'assets/images/cosmic-logo-dark-bg-monochrome.svg';
-import * as lightLogo from 'assets/images/cosmic-logo-light-bg-monochrome.svg';
+import * as logo from 'assets/images/cosmic-logo-dark-bg-monochrome.svg';
 
 const useStyles = makeStyles(() => createStyles({
   logo: {
@@ -30,14 +29,12 @@ const useStyles = makeStyles(() => createStyles({
 
 export const Logo = React.memo<{ color?: string }>(({ color }) => {
   const classes = useStyles();
-  const theme = useTheme();
-  const { mode } = theme.palette;
 
   return (
     <div className={classes.logoContainer}>
       <img
         className={classes.logo}
-        src={mode == 'light' ? lightLogo : darkLogo}
+        src={logo}
         style={{
           color,
           fill: color,
